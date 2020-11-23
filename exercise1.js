d27b8623f18a4c3f01884d7f53c9feae45df5fc1
ejinaaa <eunjink0423@gmail.com>
2020-11-23 17:06:41 +0900

    16번 보류

--- exercise1.js
+++ exercise1.js
@@ -138,12 +138,11 @@ console.log(result5);
 
 // 16번 역정삼각형 출력하기 (보류)
 let result6 = '';
-console.log('18번');
 for (let i = 0; i < line + 1; i++) {
   for (let j = 0; j < i; j++) {  // 공백 감소
     result6 += ' ';
   }
-  for (let j = line; j > i; j--) { // 별 증가
+  for (let j = line * 2; j > i * 2; j--) { // 별 증가
     result6 += '*';
   }
   result6 += '\n';
