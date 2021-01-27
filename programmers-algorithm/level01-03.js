19806b64e57ea76fa8e198bef69cc5221d014fdf
ejinaaa <eunjink0423@gmail.com>
2021-01-28 02:38:17 +0900

    feat: Complete

---
+++ programmers-algorithm/level01-03.js
@@ -0,0 +1,12 @@
+// 평균 구하기
+function solution(arr) {
+  return arr.reduce((acc, cur) => acc + cur) / arr.length;
+  // let sum = 0;
+  // for (let i = 0; i < arr.length; i++) {
+  //   sum += arr[i];
+  // }
+  // return sum / arr.length;
+}
+
+console.log(solution([1,2,3,4]));
+console.log(solution([5, 5]));
